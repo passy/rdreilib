e6fe7f311abe13bc92631d20f4009d3aa96ecd63
Pascal Hartig (Home) <phartig@rdrei.net>
2009-08-12 23:34:42 +0200

    Signature games

--- updater/meta.py
+++ updater/meta.py
@@ -8,6 +8,9 @@
  :copyright: 2008, 2009 by Pascal Hartig <phartig@rdrei.net>
  :license: GPL v3, see doc/LICENSE for more details.
  """
+
+from thirdparty import gnupg
+
 import yaml
 import hashlib
 import copy
@@ -25,11 +28,14 @@ class Meta(object):
         'package.filename',
     )
 
-    def __init__(self, filename):
+    def __init__(self, filename, gnupghome=None):
         config_content = open(filename, 'r').read()
         self.data = yaml.load(config_content)
+        if not self.data:
+            raise MetaConfigError("Loading config file %r failed!" % filename)
         self.check()
         self._verify_hash()
+        self.gpg = gnupg.GPG(gnupghome=gnupghome)
 
     def check(self):
         """Checks for required data in config, but only on two dimensions."""
@@ -39,6 +45,11 @@ class Meta(object):
                 self.data[key1][key2]
             except KeyError:
                 raise MetaConfigError("Required field '%s' missing!" % field)
+
+    def _get_package(self):
+        """Returns a read-only file objects of the referenced package."""
+
+        return open(self.data['package']['filename'], 'r')
 
     def _verify_hash(self):
         """Verifies meta integrity by its hash."""
@@ -50,15 +61,51 @@ class Meta(object):
                                      "Excpected %r, got %r." %
                                      (exp, got))
 
-    def _build_validation_hash(self):
-        """Builds the hash to verify meta signature."""
+    def _build_meta_string(self):
+        """Returns string of all hashable attributes of meta file."""
 
         hdata = copy.deepcopy(self.data)
         # Deleting data not used to build hash.
         del(hdata['meta']['hash'])
         del(hdata['meta']['signature'])
 
-        return hashlib.sha1(str(hdata)).hexdigest()
+        return str(hdata)
+
+    def _build_validation_hash(self):
+        """Builds the hash to verify meta signature."""
+
+        hdata = self._build_meta_string()
+        return hashlib.sha1(hdata).hexdigest()
+
+class MetaCreator(Meta):
+    """Helper class for creation of meta config files."""
+
+    def __init__(self, filename, gnupghome=None):
+        super(MetaCreator, self).__init__(filename, gnupghome)
+        self.file = open(filename, 'rw')
+
+    def make_signatures(self):
+        """Creates package signature, package hash, meta signature and meta
+        hash."""
+        data = {
+            'meta': {
+                'hash': self._build_validation_hash(),
+                'signature': self._sign_meta(),
+            }, 'package': {}
+        }
+
+        return data
+
+    def _sign_meta(self, keyid=None):
+        """Signs the meta data with GnuPG.
+        :param keyid str Overrides default GnuPG key.
+
+        :return string
+        """
+
+        hdata = self._build_validation_hash()
+        return self.gpg.sign(hdata, detached=True)
+
 
 class MetaConfigError(Exception):
     pass
---
+++ updater/test/test_creation2.py
@@ -0,0 +1,4 @@
+from meta import *
+
+mc = MetaCreator("test/update_1.conf")
+print "MC: %r" % mc.make_signatures()
