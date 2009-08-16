4f6ebe73031c01c419db407304e5504b45211a9c
Pascal Hartig (Home) <phartig@rdrei.net>
2009-08-16 23:02:05 +0200

    Major improvements on UI
    * SASS in background for CSS
    * First real AJAX call

---
+++ updater/shared/js/updater.js
@@ -0,0 +1,26 @@
+function UpdateApplication() {
+    bind_check();
+
+    function bind_check() {
+        $("#update_link").click(check_updates);
+    }
+
+    function check_updates() {
+        // TODO: Show a spinner somewhere!
+        $.getJSON("ajax/check_update", null,
+        function(data) {
+            if(data[0] > data[1]) {
+                load_update_table();
+            } else {
+                $("#check1").hide();
+                $("#check2").fadeIn();
+            }
+        });
+        return false;
+    }
+
+    function load_update_table() {
+        $("#post-list").load("ajax/update_skeleton");
+    }
+
+}
