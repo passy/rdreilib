7a8e06641c1d285a4c1b9b1e3eeda4e08a03962e
Pascal Hartig (Notebook) <phartig@rdrei.net>
2009-08-16 16:40:06 +0200

    Added Models

--- updater/models.py
+++ updater/models.py
@@ -8,23 +8,85 @@ Persistance layer for R3U
 :license: GPL v3, see doc/LICENSE for more details.
 """
 
+import sqlalchemy as db
+from sqlalchemy import orm
+
 from ..database import ModelBase
 from ..p2lib import P2Mixin
 
 import logging
+import datetime
 
 
 log = logging.getLogger("rdreilib.updater.models")
-
+# This represents the different states an update can have. Having the 10,
+# however, being the success state, feels a bit weird.
+UPDATE_STATES = {
+    'pending': 0,
+    'download': 1,
+    'verify': 2,
+    'unpack': 3,
+    'backup': 4,
+    'patch': 5,
+    'success': 10,
+    'failure': 20
+}
 
 class VersionLog(ModelBase):
     """Stores information on installed versions and their installation state."""
 
     __tablename__ = "updater_versionlog"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    revision = db.Column(db.Integer, unique=True)
+    long_version = db.Column(db.Unicode(15), unique=True)
+
+    last_updated = db.Column(db.DateTime)
+
+    def __init__(self, revision, long_version):
+        self.revision = revision
+        self.long_version = long_version
+        self.last_updated = datetime.datetime.now()
+
+    def __repr__(self):
+        if self.id:
+            return u"<VersionLog(%d, revision=%d, 'long_version='%s'"\
+                    ")>" (self.id, self.revision, self.long_version)
+                          
+        else:
+            return u"<VersionLog(unsafed)>"
 
 
 class UpdateLog(ModelBase):
     """Contains live data about current updating progress."""
 
-    __tablename__ "updater_updatelog"
+    __tablename__ = "updater_updatelog"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    version_id = db.Column(db.Integer, db.ForeignKey('updater_versionlog.id',
+                                                     ondelete="CASCADE"))
+    version = orm.relation(VersionLog, backref='update_log')
+
+    _state = db.Column(db.SmallInteger)
+    message = db.Column(db.Unicode(140))
+
+    def _get_state(self):
+        return self._state
+
+    def _set_state(self, value):
+        if value in UPDATE_STATES.values():
+            self._state = value
+
+        elif value in UPDATE_STATES.keys():
+            self._state = UPDATE_STATES[value]
+        
+        else:
+            raise ValueError("State must be an integer or string specified "
+                             " in UPDATE_STATES!")
+
+    state = orm.synonym('state', descriptor=property(_get_state,
+                                                     _set_state))
+
+    def __init__(self, version, state, message=None):
+        self.version = version
+        self.state = state
+        self.message = message and message or ''
 
