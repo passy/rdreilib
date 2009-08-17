f7bd3df89c9036e3bb6be91530f31689586ca91c
Pascal Hartig (Home) <phartig@rdrei.net>
2009-08-17 21:45:47 +0200

    Beaker Session and Cache integrated

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+rdreilib.exceptions
+~~~~~~~~~~~~~~~~~~~
+Exceptions used by rdreilib
+
+:copyright: 2009, Pascal Hartig <phartig@rdrei.net>
+:license: GPL v3, see doc/LICENSE for more details.
+"""
+
+class ConfigError(Exception):
+    pass
+
