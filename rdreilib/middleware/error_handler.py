c241997e9a40e0cfc1199962486fd7eef6468bca
Pascal Hartig (Home) <phartig@rdrei.net>
2009-12-17 20:44:19 +0100

    Error 500 logging

--- rdreilib/middleware/error_handler.py
+++ rdreilib/middleware/error_handler.py
@@ -11,6 +11,7 @@ Provides advanced error handling as glashammer middleware.
 
 from glashammer.utils import emit_event
 import logging
+import traceback
 
 
 class ErrorHandler(object):
@@ -26,6 +27,7 @@ class ErrorHandler(object):
         self._setup_config()
         self.log = self._get_logger()
         app.connect_event('request-error', self._handle_request_error)
+        app.connect_event('request-fatal', self._handle_request_fatal)
 
     def _setup_config(self):
         """Set up config variables in the application."""
@@ -41,13 +43,18 @@ class ErrorHandler(object):
         logging_key = self.app.cfg['error/logging_handler']
         return logging.getLogger(logging_key)
 
+    def _find_view_for_code(self, code):
+        """Generates an endpoint and tries to find a corresponding view for
+        it."""
+
+        endpoint = "error/error_{0}".format(code)
+        return self.app.view_finder.find(endpoint)
+
     def _handle_request_error(self, request, error):
         """Called by the request-error event."""
 
         self._log_error(request, error)
-
-        endpoint = "error/error_{0}".format(error.code)
-        view = self.app.view_finder.find(endpoint)
+        view = self._find_view_for_code(error.code)
 
         if view:
             emit_event('error-match', error, view)
@@ -55,6 +62,19 @@ class ErrorHandler(object):
 
             # FIXME: This way sucks hard!
             error.get_body = lambda environ: data
+
+    def _handle_request_fatal(self, request, error):
+        """Called on the request-fatal event, indicating a fatal error occured
+        while processing the current view."""
+
+        self._log_fatal(request, error)
+        view = self._find_view_for_code(500)
+
+        # In debug mode, we always prefer the original traceback.
+        if view and not self.app.cfg['general/debug']:
+            emit_event('error-match', error, view)
+
+            error['response'] = view(request)
 
     def _log_error(self, request, error):
         """Log errors in different ways."""
@@ -65,10 +85,12 @@ class ErrorHandler(object):
 
                 self.log.warn(self._format_error(request, error))
 
-            elif error.code in range(500, 599) and\
-                 self.app.cfg['error/log_5xx']:
+    def _log_fatal(self, request, error):
+        """Log fatal errors."""
 
-                self.log.error(self._format_error(request, error))
+        if self.app.cfg['error/logging_enabled'] and\
+           self.app.cfg['error/log_5xx']:
+            self.log.error(self._format_fatal(request, error))
 
     def _format_error(self, request, error):
         """Creates a logging-friendly representation of ``error``."""
@@ -78,8 +100,24 @@ class ErrorHandler(object):
             name=error.name,
             code=error.code,
             path=request.path,
-            referrer=request.referrer
-        )
+            referrer=request.referrer)
+
+    def _format_fatal(self, request, error):
+        """Creates a logging-friendly representation of a fatal ``error``."""
+
+        return "Error 500 at {path}. Referrer: {referrer}. Details: "\
+                "{traceback}".format(
+                    path=request.path,
+                    referrer=request.referrer,
+                    traceback=self._get_traceback(error))
+
+    def _get_traceback(self, error):
+        """Returns a formatted traceback for logging purposes."""
+
+        return "\n".join(traceback.format_exception(
+            error['type'],
+            error['value'],
+            error['traceback']))
 
 
 def setup_error_handler(app):
@@ -90,7 +128,7 @@ def setup_error_handler(app):
 
     Does not support correct handling of 500 errors yet.
     Adds a bunch of new configuration variables:
-    
+
     * error/logging_enabled: Allows you to disable error logging.
     * error/logging_handler: Name of the error handler to log http errors to.
     * error/log_4xx: Log errors like 400, 401, 402 ...
